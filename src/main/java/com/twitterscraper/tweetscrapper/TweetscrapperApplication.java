8227083de3987cb7684f590d574508b423e316ff
Sambuddha Chaudhuri <sambuddhachaudhuri20@gmail.com>
2021-08-23 01:35:52 +0530

    Checking in The Initial Repo with SQL Connection Properties

---
+++ src/main/java/com/twitterscraper/tweetscrapper/TweetscrapperApplication.java
@@ -0,0 +1,13 @@
+package com.twitterscraper.tweetscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetscrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetscrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitterscraper/tweetscrapper/TweetscrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitterscraper.tweetscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetscrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
