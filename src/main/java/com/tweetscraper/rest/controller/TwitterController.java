a02df7a366181cb699ddbe7d434685fe94b8dd05
Vinod Yadav <contactvinodyadav@gmail.com>
2021-09-04 11:11:04 +0530

    Added twitter account name in application.properties

--- src/main/java/com/tweetscraper/rest/controller/TwitterController.java
+++ src/main/java/com/tweetscraper/rest/controller/TwitterController.java
@@ -1,6 +1,7 @@
 package com.tweetscraper.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,16 @@ import com.tweetscraper.service.TweetScraperService;
 
 @RestController
 @RequestMapping("/test-tweet")
-public class TwiitterController {
+public class TwitterController {
 
 	@Autowired
 	private TweetScraperService tweetService;
-	
+
+	@Value("${twitter.account.name}")
+	private String accountName;
 	
 	@GetMapping
 	public void getLatestTweetIntoDatabase() {
-		tweetService.findTweets("twitterscraper");
+		tweetService.findTweets(accountName);
 	}
 }
--- src/main/java/com/tweetscraper/scheduletasks/TweetScraperScheduleTask.java
+++ src/main/java/com/tweetscraper/scheduletasks/TweetScraperScheduleTask.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import com.tweetscraper.config.TwitterTemplateCreator;
+import com.tweetscraper.service.TweetScraperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.social.twitter.api.CursoredList;
 import org.springframework.social.twitter.api.Twitter;
@@ -16,47 +18,17 @@ import com.tweetscraper.repository.TwitterUserRepository;
 @Component
 public class TweetScraperScheduleTask {
 
-	@Autowired
-	private TwitterUserRepository tweetUserRepository;
-	
-	@Autowired
-	private TwitterTemplateCreator twitterTemplateCreator;
+    @Autowired
+    private TweetScraperService tweetScraperService;
 
-	// 1hr interval between invocations measured from the completion of the task
-	// 1hr = 60*60*1000
-	//@Scheduled(fixedDelay = 60*60*1000)
-	@Scheduled(fixedDelay = 5000)
-	public void scrapeTweets() {
-	/*
-		TweeterUserEntity user = new TweeterUserEntity();
-		user.setName(randomString(10));
-		user.setEmail(randomString(20));
+    @Value("${twitter.account.name}")
+    private String accountName;
 
-		tweetUserRepository.save(user);
-	*/
-	}
-
-	private String randomString(Integer length) {
-		byte[] array = new byte[length]; // length is bounded by 7
-		new Random().nextBytes(array);
-		return new String(array, Charset.forName("UTF-8"));
-	}
-	
-	public ArrayList<Long> getIdList(String userName) {
-
-		Twitter twitter = twitterTemplateCreator.getTwitterTemplate(userName);
-		long cursor = -1;
-		CursoredList<Long> listId;
-		ArrayList<Long> idList = new ArrayList<>();
-
-		do {
-			listId = twitter.friendOperations().getFollowerIdsInCursor("Riddhikgunners", cursor);
-			for (Long ids : listId) {
-				idList.add(ids);
-			}
-
-		} while ((cursor = listId.getNextCursor()) != 0);
-
-		return idList;
-	}
+    // 1hr interval between invocations measured from the completion of the task
+    // 1hr = 60*60*1000
+    //@Scheduled(fixedDelay = 60*60*1000)
+    @Scheduled(fixedDelay = 5000)
+    public void scrapeTweets() {
+//        tweetScraperService.findTweets(accountName);
+    }
 }
