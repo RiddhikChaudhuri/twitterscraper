24518211c069760fab6f0d21770a46716a46e279
Sambuddha Chaudhuri <sambuddhachaudhuri20@gmail.com>
2021-09-05 11:56:30 +0530

    Twitter 4J Branch Changes

--- src/main/java/com/tweetscraper/scheduletasks/TweetScraperScheduleTask.java
+++ src/main/java/com/tweetscraper/scheduletasks/TweetScraperScheduleTask.java
@@ -1,10 +1,14 @@
 package com.tweetscraper.scheduletasks;
 
-import com.tweetscraper.service.TweetScraperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.tweetscraper.service.TweetScraperService;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 @Component
 public class TweetScraperScheduleTask {
@@ -17,9 +21,12 @@ public class TweetScraperScheduleTask {
 
     // 1hr interval between invocations measured from the completion of the task
     // 1hr = 60*60*1000
-    @Scheduled(fixedDelay = 60 * 60 * 1000)
-//    @Scheduled(fixedDelay = 5000)
+   // @Scheduled(fixedDelay = 60 * 60 * 1000)
+    @Scheduled(fixedDelay = 5000)
     public void scrapeTweets() {
-//        tweetScraperService.findTweets(accountName);
+    	
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        tweetScraperService.findTweets(accountName);
     }
 }
